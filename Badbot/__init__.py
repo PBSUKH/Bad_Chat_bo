f3de875f57c04a77f60a42f3b51e5dd01258e3ad
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-18 12:15:14 +0530

    Update __init__.py

--- Badbot/__init__.py
+++ Badbot/__init__.py
@@ -6,7 +6,27 @@ from os import listdir, path
 from dotenv import load_dotenv
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, BOT_USERNAME, OWNER_ID, GPT_API, LOGGER_ID, DEEP_API, SUDO_USER
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
 
+# the secret configuration specific things
+from config import DB_URI
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+try:
+    BASE = declarative_base()
+    SESSION = start()
+except AttributeError as e:
+    # this is a dirty way for the work-around required for #23
+    print("DB_URI is not configured. Features depending on the database might have issues.")
+    print(str(e))
+    
 
 
 
