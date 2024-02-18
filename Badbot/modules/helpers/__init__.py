ef3890fd11867574f91fe09eb3fb9a662515d1b9
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-18 14:06:14 +0530

    Create __init__.py

---
+++ Badbot/modules/helpers/__init__.py
@@ -0,0 +1,22 @@
+from typing import Callable
+
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import Message
+
+from Badbot import OWNER_ID, 
+
+
+def is_admins(func: Callable) -> Callable:
+    async def non_admin(c: rankbot, m: Message):
+        if m.from_user.id == OWNER_ID:
+            return await func(c, m)
+
+        admin = await c.get_chat_member(m.chat.id, m.from_user.id)
+        if admin.status in [ChatMemberStatus.OWNER_ID, ChatMemberStatus.ADMINISTRATOR]:
+            return await func(c, m)
+
+    return non_admin
+
+
+from .inline import *
+from .read import *
