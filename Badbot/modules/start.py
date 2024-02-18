c1f3f2ba75e0ce9ad2a61579a9237c46150152f4
SUKH ❤️ <158456267+PBSUKH@users.noreply.github.com>
2024-02-18 16:23:48 +0530

    Update start.py

--- Badbot/modules/start.py
+++ Badbot/modules/start.py
@@ -4,8 +4,6 @@ from config import BOT_USERNAME, OWNER_ID
 from pyrogram.types import InlineKeyboardButton as ib
 import asyncio
 from Badbot import app
-from Badbot.modules.helpers.chats import add_served_chat
-from Badbot.modules.helpers.users import add_served_user
 from Badbot.modules.helpers import (
     CLOSE_BTN,
     DEV_OP,
